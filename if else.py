3a2455875639d9d70a8152ca7cb61ca2d3e98d1e
Muhammad Muddassir Hasni <142342909+ItsCoderz@users.noreply.github.com>
2023-12-16 19:21:58 +0500

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,10 @@
+a = int(input("Enter Kamran Akmal scores"))
+b = int(input("Enter Shoaib Akhtar scores"))
+c = int(input("Enter Shahid Afridi scores"))
+if a > 30:
+    print ("Kamran Akmal scores 30 above,")
+elif b <=30:
+    print ("Shoaib Akhtar scores  ,")
+else:    
+    print ("Shahid Afridi scores betwwen 10 < 20,") 
+    
---
+++ slicing.py
@@ -0,0 +1,16 @@
+var='SOFTWAREENGINEERING'
+s=var[15:20]
+print(s)
+
+var='SOFTWAREENGINEERING'
+s=var[8:14]
+print(s)
+
+var='SOFTWAREENGINEERING'
+print(var[-17:-20:-1])
+
+var='SOFTWAREENGINEERING'
+print(var[-1:-12:-1])
+
+var='SOFTWAREENGINEERING'
+print(var[-10:-16:-1])
